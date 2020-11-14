c99c3d4dd80e4a56a0df3e6eab9b3b352cbeed7a
GithubTiger <58816240+karol71927@users.noreply.github.com>
2020-11-14 19:52:27 +0100

    Dodaj, wyszukaj

--- src/app/src/main/java/BazaKsiazek.java
+++ src/app/src/main/java/BazaKsiazek.java
@@ -11,16 +11,18 @@ public class BazaKsiazek {
         String autor = scanner.next();
         System.out.print("Podaj tytuł książki: ");
         String tytul = scanner.next();
-        System.out.print("Podaj IBSN");
-        String IBSN = scanner.next();
+        System.out.print("Podaj wydawnictwo: ");
+        String wydawnictwo = scanner.next();
+        System.out.print("Podaj ISBN");
+        String ISBN = scanner.next();
 
         try{
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql//localhost:3306/biblioteka",
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bibliotekadb",
                     "root",
-                    "root");
+                    "");
             Statement statement = con.createStatement();
-            statement.executeUpdate("INSERT INTO ksiazka(Autor, Tytul, IBSN, Wydawnictwo, Status) VALUES (" + "'" + autor + "','" + tytul + "','" + IBSN +"'," + "1)");
+            statement.executeUpdate("INSERT INTO ksiazka(Autor, Tytul, ISBN, Wydawnictwo, Status) VALUES (" + "'" + autor + "','" + tytul + "','" + wydawnictwo +"','" + ISBN +"'," + "1)");
 
 
         }catch (Exception e) {
@@ -35,26 +37,26 @@ public class BazaKsiazek {
     public String[] wyszukaj(String tytul, String autor) throws Exception{
         String[] ksiazka = new String[5];
         try{
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql//localhost:3306/bibliotekadb",
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bibliotekadb",
                     "root",
-                    "root");
+                    "");
             Statement statement = con.createStatement();
-            String sql = "SELECT Tytul, Autor, IBSN, Wydawnictwo, Status FROM Ksiazka WHERE Tytul = '";
+            String sql = "SELECT Tytul, Autor, ISBN, Wydawnictwo, Status FROM Ksiazka WHERE Tytul = '";
             sql = sql + (tytul + "' AND Autor = '" + autor + "';");
+            System.out.println(sql);
             ResultSet rs = statement.executeQuery(sql);
-            if (!rs.next())
-                throw new Exception("Nie ma książki w bazie");
             while (rs.next()){
-                ksiazka[0] = rs.getString(0);
-                ksiazka[1] = rs.getString(1);
-                ksiazka[2] = rs.getString(2);
-                ksiazka[3] = rs.getString(3);
-                ksiazka[4] = rs.getString(4);
+                ksiazka[0] = rs.getString("Tytul");
+                ksiazka[1] = rs.getString("Autor");
+                ksiazka[2] = rs.getString("ISBN");
+                ksiazka[3] = rs.getString("Wydawnictwo");
+                ksiazka[4] = rs.getString("Status");
             }
             return ksiazka;
         }catch (Exception e) {
             System.out.println(e.toString());
+            e.printStackTrace();
         }
         return null;
     }
