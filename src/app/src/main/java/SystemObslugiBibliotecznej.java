07731113fb596e5182fc1f71b5104806af88396d
GithubTiger <58816240+karol71927@users.noreply.github.com>
2020-11-17 00:43:02 +0100

    Klasa SystemObslugiBibliotecznej

--- src/app/src/main/java/SystemObslugiBibliotecznej.java
+++ src/app/src/main/java/SystemObslugiBibliotecznej.java
@@ -16,9 +16,9 @@ public class SystemObslugiBibliotecznej {
         int ID_bibliotekarza;
         while (true) {
             System.out.print("Podaj login: ");
-            String login = scanner.next();
+            login = scanner.next();
             System.out.print("Podaj haslo: ");
-            String haslo = scanner.next();
+            haslo = scanner.next();
             String loginBaza = new String();
             String hasloBaza = new String();
             try {
@@ -70,9 +70,54 @@ public class SystemObslugiBibliotecznej {
         System.exit(0);
     }
 
+    public void zarejestruj(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Zarejestruj nowego użytkownika");
+        System.out.print("Podaj imię: ");
+        String imie = scanner.next();
+        System.out.print("Podaj nazwisko: ");
+        String nazwisko = scanner.next();
+        System.out.print("Podaj pesel: ");
+        String pesel = scanner.next();
+        System.out.print("Podaj login: ");
+        String login = scanner.next();
+        System.out.print("Podaj haslo: ");
+        String haslo = scanner.next();
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bibliotekadb",
+                    "root",
+                    "");
+            Statement statement = con.createStatement();
+            while (true) {
+                String sql = "SELECT login FROM System WHERE login = '" + login + "';";
+                ResultSet rs = statement.executeQuery(sql);
+                if (rs.next()) {
+                    System.out.println("Istnieje konto o podanym loginie, podaj inny");
+                    System.out.print("Podaj login: ");
+                    login = scanner.next();
+                } else {
+                    break;
+                }
+            }
+            String sql = "INSERT INTO uzytkownik (Imie, Nazwisko, Pesel, Status_konta) VALUES ('" + imie + "','" + nazwisko + "','" + pesel + "', 1);";
+            statement.executeUpdate(sql);
+            sql = "SELECT ID_Uzytkownika FROM uzytkownik WHERE IMIE = '" + imie + "' AND Nazwisko = '" + nazwisko + "';";
+            ResultSet rs = statement.executeQuery(sql);
+            int id = -1;
+            if (rs.next())
+                id = rs.getInt("ID_uzytkownika");
+            sql = "INSERT INTO System (Login, Haslo, Typ_konta, ID_uzytkownika) VALUES ('" + login + "','" + haslo + "','1'," + id + ");";
+            statement.executeUpdate(sql);
+        }catch (Exception e){
+            System.out.println(e.toString());
+        }
+        System.out.println("Zarejestrowano pomyślnie");
+    }
+
     public static void main(String[] args) {
         SystemObslugiBibliotecznej system = new SystemObslugiBibliotecznej();
-
+        system.zarejestruj();
         system.zaloguj();
         system.wyloguj();
     }
