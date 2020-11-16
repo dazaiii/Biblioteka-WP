5d4f1208539916f0964256b8deb071d3707edb28
Kinga <58816347+dazaiii@users.noreply.github.com>
2020-11-16 23:57:05 +0100

    klasa Bibliotekarz

--- src/app/src/main/java/Bibliotekarz.java
+++ src/app/src/main/java/Bibliotekarz.java
@@ -1,2 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Bibliotekarz {
+    private int IdentyfikatorPracownika;
+    private String imie;
+    private String nazwisko;
+    private String pesel;
+
+    public Bibliotekarz(int id) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bibliotekadb",
+                    "root",
+                    "");
+            Statement statement = con.createStatement();
+            String sql = "SELECT imie, nazwisko, pesel FROM Uzytkownik";
+            ResultSet rs = statement.executeQuery(sql);
+            while(rs.next()) {
+                this.imie = rs.getString("Imie");
+                this.nazwisko = rs.getString("Nazwisko");
+                this.pesel = rs.getString("Pesel");
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
 }
