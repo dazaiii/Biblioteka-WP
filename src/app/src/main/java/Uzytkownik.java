8cb7d3ebd0d2f38f0d4c120c8d34401e8ee547e2
Kinga <58816347+dazaiii@users.noreply.github.com>
2020-11-16 23:08:44 +0100

    Menu w klasie użytkownik

--- src/app/src/main/java/Uzytkownik.java
+++ src/app/src/main/java/Uzytkownik.java
@@ -7,7 +7,6 @@ public class Uzytkownik {
     private Konto konto;
 
     public Uzytkownik(int id) {
-
         this.imie = imie;
         this.nazwisko = nazwisko;
         this.pesel = pesel;
@@ -18,28 +17,36 @@ public class Uzytkownik {
         System.out.println("1. Sprawdź wypożyczone książki");
         System.out.println("2. Sprzwdź zarezerwowane książki");
         System.out.println("3. Przedłuż ważność konta");
-
+        System.out.println("4. Wyszukaj ksiazke");
+        Konto konto1 = new Konto(imie, nazwisko);
+        BazaKsiazek bazaKsiazek = new BazaKsiazek();
         Scanner scanner = new Scanner(System.in);
         int wybor = scanner.nextInt();
         while (true) {
             switch (wybor) {
                 case 1: {
-
+                    konto1.sprawdzWypozyczone(imie, nazwisko);
                 }
                 case 2: {
-
+                    konto1.sprawdzRezerwacje(imie, nazwisko);
                 }
                 case 3: {
-
+                    konto1.przedluzWaznoscKonta(imie, nazwisko);
                 }
                 case 4: {
-
+                    String autor, tytul;
+                    System.out.print("Podaj autora ksiazki: ");
+                    Scanner a = new Scanner(System.in);
+                    autor = a.toString();
+                    System.out.println("Podaj tytul ksiazki: ");
+                    Scanner t = new Scanner(System.in);
+                    tytul = t.toString();
+                    bazaKsiazek.wyszukaj(autor, tytul);
                 }
                 default: {
                     System.out.println("Podano niewłąściwą opcję");
                 }
             }
         }
-
     }
 }
