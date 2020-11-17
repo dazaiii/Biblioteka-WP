4e07587f3f83834c5fc86717536d9803c28e61b3
Kinga <58816347+dazaiii@users.noreply.github.com>
2020-11-17 17:43:29 +0100

    Klasa Książka

--- src/app/src/main/java/Ksiazka.java
+++ src/app/src/main/java/Ksiazka.java
@@ -11,10 +11,29 @@ public class Ksiazka {
         this.wydawnictwo = wydawnictwo;
     }
 
+    public void opis(String nazwa, String autor){
+        BazaKsiazek bazaKsiazek = new BazaKsiazek();
+        String[] ksiazka = bazaKsiazek.wyszukaj(nazwa,autor);
+        System.out.println("Autorem książki " + ksiazka[0] + " jest " + ksiazka[1]);
+        System.out.println("Jej ISBN to " + ksiazka[2]);
+        System.out.println("Jest wydawana przez wydawnictwo: " + ksiazka[3]);
+        if(ksiazka[4].equals("0")){
+            System.out.println("Książka jest aktualnie dostępna");
+        }
+        else {
+            if(ksiazka[4].equals("1")) {
+                System.out.println("Książka jest aktualnie niedostępna");
+                System.out.println("Będzie dostępna od " + ksiazka[5]);
+            }
+            else
+                System.out.println("Książka jest aktualnie niedostępna");
+        }
+    }
+
     public boolean status(String nazwa, String autor){
-        // za pomocą select w bazie sprawdź dostępność książki
-        // do tego celu można wykorzystać funkcję wyszukaj z klasy BazaKsiazek
-        if (1 == 1){
+        BazaKsiazek bazaKsiazek = new BazaKsiazek();
+        String[] wyn = bazaKsiazek.wyszukaj(nazwa,autor);
+        if (wyn[4].equals("1")){
             return true;
         }
         return false;
@@ -24,16 +43,37 @@ public class Ksiazka {
         if ( !status(nazwa,autor) ){
             throw new Exception("Wybrana książka nie istnije lub jest niedostępna");
         }
-        // po sprawdzeniu że jest wyszukać ją w bazie zrobić update ilości i utworzyć obiekt do zwrócenia
-        Ksiazka K = new Ksiazka(nazwa, tytul, "a", "A");
-
+        BazaKsiazek bazaKsiazek = new BazaKsiazek();
+        String[] ksiazka = bazaKsiazek.wyszukaj(nazwa,autor);
+        Ksiazka K = new Ksiazka(ksiazka[0], ksiazka[1], ksiazka[2], ksiazka[3]);
+        bazaKsiazek.zaktualizuj(nazwa, autor, 1);
         return K;
     }
 
-    public boolean rezerwuj(String nazwa, String autor){
+    public boolean rezerwuj(String nazwa, String autor) throws Exception{
+        if ( !status(nazwa,autor) ){
+            throw new Exception("Wybrana książka nie istnije lub jest niedostępna");
+        }
+        BazaKsiazek bazaKsiazek = new BazaKsiazek();
+        bazaKsiazek.zaktualizuj(nazwa, autor, 2);
         return true;
     }
 
+    public boolean zwroc(String nazwa, String autor) throws Exception{
+        if ( !status(nazwa,autor) ){
+            throw new Exception("Wybrana książka nie istnije lub jest niedostępna");
+        }
+        BazaKsiazek bazaKsiazek = new BazaKsiazek();
+        bazaKsiazek.zaktualizuj(nazwa, autor, 0);
+        return true;
+    }
 
-
+    public boolean przedluzWypozyczenie(String nazwa, String autor) throws Exception{
+        if ( !status(nazwa,autor) ){
+            throw new Exception("Wybrana książka nie istnije lub jest niedostępna");
+        }
+        BazaKsiazek bazaKsiazek = new BazaKsiazek();
+        bazaKsiazek.zaktualizuj(nazwa,autor,1);
+        return true;
+    }
 }
