3ba86f639fd3ba8ad09624e83a9c86848e6fed64
GithubTiger <58816240+karol71927@users.noreply.github.com>
2020-12-08 01:08:24 +0100

    Prototyp

---
+++ Wzorzec kreacyjny prototyp/src/Konto.java
@@ -0,0 +1,68 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class Konto {
+    private int StatusKonta;
+    private int WypozyczoneKsiazki;
+    private int ZarezerwowaneKsiazki;
+
+    public Konto(String imie, String nazwisko){
+        BazaKsiazek bazaKsiazek = new BazaKsiazek();
+        this.WypozyczoneKsiazki = bazaKsiazek.wyszukaj(imie,nazwisko,0).size();
+        this.ZarezerwowaneKsiazki = bazaKsiazek.wyszukaj(imie,nazwisko,2).size();
+    }
+
+    public int sprawdzWypozyczone(String imie, String nazwisko) {
+        BazaKsiazek bazaKsiazek = new BazaKsiazek();
+        ArrayList<String> lista = new ArrayList<>();
+        int ileKsiazek = 0;
+        lista = bazaKsiazek.wyszukaj(imie, nazwisko, 0);
+        for(int i = 0; i < lista.size(); i+=4) {
+            ileKsiazek++;
+        }
+        for(int i = 0; i < lista.size(); i+=4) {
+            for(int j = i; j < i+4; j++) {
+                System.out.print(lista.get(j) + " ");
+            }
+            System.out.println();
+        }
+        return ileKsiazek;
+    }
+
+    public int sprawdzRezerwacje(String imie, String nazwisko) {
+        BazaKsiazek bazaKsiazek = new BazaKsiazek();
+        ArrayList<String> lista = new ArrayList<>();
+        int ileKsiazek = 0;
+        lista = bazaKsiazek.wyszukaj(imie, nazwisko, 2);
+        for(int i = 0; i < lista.size(); i+=4) {
+            ileKsiazek++;
+        }
+        for(int i = 0; i < lista.size(); i+=4) {
+            for(int j = i; j < i+4; j++) {
+                System.out.print(lista.get(j) + " ");
+            }
+            System.out.println();
+        }
+        return ileKsiazek;
+    }
+
+    public void przedluzWaznoscKonta(String imie, String nazwisko) {
+        if(StatusKonta == 0) {
+            try {
+                StatusKonta = 1;
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bibliotekadb",
+                        "root",
+                        "");
+                Statement statement = con.createStatement();
+                statement.executeUpdate("UPDATE Uzytkownik SET Uzytkownik.Status_konta = 1 WHERE Uzytkownik.imie = '" + imie + "' AND Uzytkownik.nazwisko = '" + nazwisko + "' ;");
+                con.close();
+            } catch(Exception e) {
+                System.out.println(e.toString());
+            }
+        }
+    }
+
+}
---
+++ Wzorzec kreacyjny prototyp/src/KsiazkaKlon.java
@@ -0,0 +1,3 @@
+public interface KsiazkaKlon {
+    Ksiazka klonuj();
+}
